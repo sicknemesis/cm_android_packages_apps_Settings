023cd95f66366d770d754610975ee6cc49d2d20d
Sicknemesis <hulluhalonen10@gmail.com>
2014-10-07 20:27:43 +0300

    Add some Cm settings to own submenu. Add little bit of rasta!
    Change-Id: I7c1ec6a3990b899c3e75ad4acd97831ac004d7db

--- src/com/android/settings/rascarlo/SystemSettings.java
+++ src/com/android/settings/rascarlo/SystemSettings.java
@@ -1,26 +1,23 @@
-
 package com.android.settings.rascarlo;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.preference.Preference;
+import android.preference.PreferenceScreen;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
-public class SystemSettings extends SettingsPreferenceFragment implements
-        Preference.OnPreferenceChangeListener {
-    private static final String TAG = "SystemSettings";
+import java.util.List;
+
+public class SystemSettings extends SettingsPreferenceFragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.system_settings);
-        }
-
-    @Override
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        // TODO Auto-generated method stub
-        return false;
     }
 }
